b034615df1bf7c5af472e9313f06ac19ffbac4c5
njohanne <aleksey88.avt@gmail.com>
2025-04-12 16:58:49 +0300

    add_kafka_user_update

--- generate.sh
+++ generate.sh
@@ -3,3 +3,7 @@ protoc  --go_out=./  \
         search.proto
 
 protoc --doc_out=. --doc_opt=markdown,README.md ./search.proto
+
+protoc  --go_out=./  \
+        --go-grpc_out=./ \
+        newUser.proto
